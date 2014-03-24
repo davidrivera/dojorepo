61018776b9f23cfc74d0a0e1f12519c64937e3ce
Daniel Lewis <daniel@levelsetlabs.com>
2014-03-23 19:52:42 -0700

    auto

--- tests/tetris_ai_test.js
+++ tests/tetris_ai_test.js
@@ -1,13 +1,35 @@
 var assert = require("assert")
 var TetrisAI = require('../lib/tetris_ai')
 
+function makeMap() {
+    var map=[]
+    for (var i=0; i<arguments.length; i++) {
+        for(var j=0; j<arguments[i].length; j++) {
+
+        }
+    }
+}
+
 describe('TetrisAI', function(){
   describe('Object itself', function(){
     it('TetrisAI can be instantiated and is an object', function(){
-        var ai = new TetrisAI();
-        console.log(typeof ai)
+        var ai = new TetrisAI()
         assert(typeof ai == "object")
-        assert(ai.prototype == TetrisAI)
     })
+
+    it('Should instantiate an object with type TetrisAI', function(){
+        var ai = new TetrisAI()
+        assert(ai instanceof TetrisAI)
+    })
+  })
+
+  it('', function() {
+    var ai = new TetrisAI()
+    var map = 5;
+    ai.decideMove(map)
+  })
+
+  it('', function() {
+
   })
 })
