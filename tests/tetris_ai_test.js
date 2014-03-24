db09becde6cddc49ef41cc46deec2bfaf9e204a6
Daniel Lewis <daniel@levelsetlabs.com>
2014-03-23 20:02:54 -0700

    auto

--- tests/tetris_ai_test.js
+++ tests/tetris_ai_test.js
@@ -1,39 +1,51 @@
-var assert = require("assert")
-var TetrisAI = require('../lib/tetris_ai')
+var assert = require("assert");
+var TetrisAI = require('../lib/tetris_ai');
 
 function makeMap() {
-    var map=[]; 
+    var map=[];
     for (var i=0; i<arguments.length; i++) {
-        var row = []; 
+        var row = [];
         for(var j=0; j<arguments[i].length; j++) {
-           row.push(arguments[i][j]); 
+           row.push(arguments[i][j]);
         }
-        map.push(row); 
+        map.push(row);
     }
-    return map; 
+    return map;
 }
 
 describe('TetrisAI', function(){
   describe('Object itself', function(){
     it('TetrisAI can be instantiated and is an object', function(){
-        var ai = new TetrisAI()
-        assert(typeof ai == "object")
-    })
+        var ai = new TetrisAI();
+        assert(typeof ai == "object");
+    });
 
     it('Should instantiate an object with type TetrisAI', function(){
-        var ai = new TetrisAI()
-        assert(ai instanceof TetrisAI)
-    })
-  })
+        var ai = new TetrisAI();
+        assert(ai instanceof TetrisAI);
+    });
+  });
 
-  it('#decideMove', function() {
-    var ai = new TetrisAI()
-    var map = 5;
+  it('#decideMove returns a result that isnt undefined', function() {
+    var ai = new TetrisAI();
+    var map = makeMap("   ");
     var move = ai.decideMove(map); 
-    assert(typeof move != "undefined"); 
-  })
+    assert(typeof move != "undefined");
+  });
+
+  it('#decideMove should choose the bottom left corner if the map is empty', function() {
+    var ai = new TetrisAI();
+    var map = makeMap(
+        "      ",
+        "      ",
+        "      ",
+        "      ",
+        "      ");
+    var move = ai.decideMove(map);
+
+  });
 
   it('', function() {
 
-  })
-})
+  });
+});
